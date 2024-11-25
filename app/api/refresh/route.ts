ce45c5647b8e46c922a7ffa4b9a6c2ce16759afc
hac1yev <ilkinhaciyev955@gmail.com>
2024-11-25 15:20:55 +0400

    test refresh token

--- app/api/refresh/route.ts
+++ app/api/refresh/route.ts
@@ -7,7 +7,7 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
     const cookie = req.headers.get("cookie");
 
-    const refreshToken = cookie?.split("=")[1] || "";
+    const refreshToken = cookie?.split("=").at(-1) || "";
 
     const isValidRefreshToken = await verifyRefreshToken(refreshToken);
     
