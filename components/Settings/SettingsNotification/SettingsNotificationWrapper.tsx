4a67dbc4b4d0bfc3f2fff4cb27549239357a4b69
hac1yev <ilkinhaciyev955@gmail.com>
2024-11-25 15:49:07 +0400

    notification settings ui

--- components/Settings/SettingsNotification/SettingsNotificationWrapper.tsx
+++ components/Settings/SettingsNotification/SettingsNotificationWrapper.tsx
@@ -3,10 +3,28 @@
 import { Box, Button, Divider, Switch, Typography } from '@mui/material';
 import HelpIcon from '@mui/icons-material/Help';
 import './SettingsNotification.css';
+import { useState } from 'react';
 
 const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
 const SettingsNotificationWrapper = () => {
+    const [notificationSettings,setNotificationSettings] = useState({
+        assignTask: true,
+        addComment: true,
+        subTask: true,
+        likeComment: true,
+        modifyTask: true,
+        addTimeline: true,
+        addUser: true
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNotificationSettings({
+          ...notificationSettings,
+          [event.target.name]: event.target.checked,
+        });
+    };
+
     return (
         <Box sx={{ width: "100%", bgcolor: '#fff', maxWidth: { sm: "100%", md: "1700px" }, p: 2 }}>
             <Box>
@@ -17,59 +35,94 @@ const SettingsNotificationWrapper = () => {
             </Box>
             <Divider sx={{ mt: 3, mb: 2 }} />
             <Box className="setting-notifications-wrapper" component={"form"} sx={{ mb: 2 }}>
-                <Box className="flex-between" sx={{ alignItems: 'start' }}>
+                <Box className="flex-between notification-list">
                     <Typography variant="h6">Enable Assign Tasks Notifications</Typography>
                     <Box className="flex-center" sx={{ gap: 1 }}>
                         <HelpIcon sx={{ color: '#494949' }} />
-                        <Switch {...label} defaultChecked />
+                        <Switch 
+                            {...label} 
+                            checked={notificationSettings.assignTask}
+                            name="assignTask"
+                            onChange={handleChange} 
+                        />
                     </Box>
                 </Box>
                 <Divider sx={{ my: 2 }} />
-                <Box className="flex-between" sx={{ alignItems: 'start' }}>
+                <Box className="flex-between notification-list">
                     <Typography variant="h6">Enable Add Comments Notifications</Typography>
                     <Box className="flex-center" sx={{ gap: 1 }}>
                         <HelpIcon sx={{ color: '#494949' }} />
-                        <Switch {...label} defaultChecked />
+                        <Switch 
+                            {...label} 
+                            checked={notificationSettings.addComment} 
+                            name="addComment"
+                            onChange={handleChange} 
+                        />
                     </Box>
                 </Box>
                 <Divider sx={{ my: 2 }} />
-                <Box className="flex-between" sx={{ alignItems: 'start' }}>
+                <Box className="flex-between notification-list">
                     <Typography variant="h6">Enable Sub-Tasks Notifications</Typography>
                     <Box className="flex-center" sx={{ gap: 1 }}>
                         <HelpIcon sx={{ color: '#494949' }} />
-                        <Switch {...label} defaultChecked />
+                        <Switch 
+                            {...label}  
+                            checked={notificationSettings.subTask}
+                            name="subTask"
+                            onChange={handleChange} 
+                        />
                     </Box>
                 </Box>
                 <Divider sx={{ my: 2 }} />
-                <Box className="flex-between" sx={{ alignItems: 'start' }}>
+                <Box className="flex-between notification-list">
                     <Typography variant="h6">Enable Like Comments Notifications</Typography>
                     <Box className="flex-center" sx={{ gap: 1 }}>
                         <HelpIcon sx={{ color: '#494949' }} />
-                        <Switch {...label} defaultChecked />
+                        <Switch 
+                            {...label}  
+                            checked={notificationSettings.likeComment} 
+                            name="likeComment"
+                            onChange={handleChange} 
+                        />
                     </Box>
                 </Box>
                 <Divider sx={{ my: 2 }} />
-                <Box className="flex-between" sx={{ alignItems: 'start' }}>
+                <Box className="flex-between notification-list">
                     <Typography variant="h6">Enable Modified Tasks Notifications</Typography>
                     <Box className="flex-center" sx={{ gap: 1 }}>
                         <HelpIcon sx={{ color: '#494949' }} />
-                        <Switch {...label} defaultChecked />
+                        <Switch 
+                            {...label} 
+                            checked={notificationSettings.modifyTask} 
+                            name="modifyTask"
+                            onChange={handleChange} 
+                        />
                     </Box>
                 </Box>
                 <Divider sx={{ my: 2 }} />
-                <Box className="flex-between" sx={{ alignItems: 'start' }}>
+                <Box className="flex-between notification-list">
                     <Typography variant="h6">Enable Task Activities Notifications</Typography>
                     <Box className="flex-center" sx={{ gap: 1 }}>
                         <HelpIcon sx={{ color: '#494949' }} />
-                        <Switch {...label} defaultChecked />
+                        <Switch 
+                            {...label} 
+                            checked={notificationSettings.addTimeline} 
+                            name="addTimeline"
+                            onChange={handleChange} 
+                        />
                     </Box>
                 </Box>
                 <Divider sx={{ my: 2 }} />
-                <Box className="flex-between" sx={{ alignItems: 'start' }}>
+                <Box className="flex-between notification-list">
                     <Typography variant="h6">Enable New User Notifications</Typography>
                     <Box className="flex-center" sx={{ gap: 1 }}>
                         <HelpIcon sx={{ color: '#494949' }} />
-                        <Switch {...label} defaultChecked />
+                        <Switch 
+                            {...label} 
+                            checked={notificationSettings.addUser} 
+                            name="addUser"
+                            onChange={handleChange} 
+                        />
                     </Box>
                 </Box>
                 <Divider sx={{ my: 2 }} />
